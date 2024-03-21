e8e8beda9c7a785481985572b0e3e5f025184f22
riskanopiyanti <114473650+riskanopiyanti@users.noreply.github.com>
2024-03-21 13:14:45 +0700

    Create App.java

---
+++ App.java
@@ -0,0 +1,13 @@
+import bangundatar.*;
+import shape.*;
+
+public class App{
+
+    public static void main(String[] args) {
+        persegi A =new persegi(sisi:5);
+        square B = new Square(side:5);
+
+        A.getInfo();
+        B.getInfo();
+    }
+}
